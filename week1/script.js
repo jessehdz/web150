1689af54c28f1f9f13aec999b3f8706559f7b1ea
Jesse <hdzj_@github.com>
2018-01-06 21:55:12 -0800

    First commit

---
+++ week1/script.js
@@ -0,0 +1,7 @@
+var firstName = "Jesse";
+
+var lastName = "Hernández";
+
+var yearsOfStudy = "two years";
+
+var goal = "finish school in the Fall and start my career on a high note this time next year";
